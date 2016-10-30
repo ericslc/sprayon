1b916ffcf582a2ba920744f1c9a7dca469699503
ericslc <eric.leatham@gmail.com>
2016-10-29 22:25:17 -0600

    updated files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-angular.module('spray', [])
+angular.module('spray', ['ui-router'])
+
+angular.module('spray', .config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: './views/home.html',
+      controller: 'sprayctrl',
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: './views/about.html',
+      controller: 'sprayctrl'
+    })
+    .state('product1', {
+      url: '/product1',
+      templateUrl: './views/product1.html',
+      controller: 'sprayctrl'
+    })
+    .state('product2', {
+      url: '/product2',
+      templateUrl: './views/product2/html',
+      controller: 'sprayctrl'
+    })
+    .state('product3', {
+      url: '/product3',
+      templateUrl: './views/product3.html',
+      controller: 'sprayctrl'
+    })
+    .state('ourwork', {
+      url: '/ourwork',
+      templateUrl:'./views/ourwork.html',
+      controller: 'sprayctrl'
+    })
+    $urlRouterProvider.otherwise('/');
+});
