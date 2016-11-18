943e50c461118112400ad2b26e6d82ba55602286
ericslc <eric.leatham@gmail.com>
2016-11-18 15:18:57 -0700

    final complete project

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,6 @@
 angular.module('spray').controller('sprayctrl', function($scope){
 $scope.test='test';
+$scope.isNavCollapsed = true;
+$scope.isCollapsed = false;
+$scope.isCollapsedHorizontal = false;
 })
